dc4a947f12e314c7728ab7e9d77312f210c364d9
S1monTheP1eMan <70568227+S1monTheP1eMan@users.noreply.github.com>
2021-01-12 11:01:54 +0200

    Init

---
+++ Assets/Scrpts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    private int _minHealth = 0;
+    private int _maxHealth = 100;
+
+    public int Health => _health;
+
+    public void TakeDamage()
+    {
+        if (_health > _minHealth)
+        {
+            _health -= 10;
+        }
+
+        Debug.Log(_health);
+    }
+
+    public void Heal()
+    {
+        if (_health < _maxHealth)
+        {
+            _health += 10;
+        }
+        
+        Debug.Log(_health);
+    }
+}
